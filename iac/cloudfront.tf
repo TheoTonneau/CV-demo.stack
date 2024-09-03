94ae476ed0812755415b049639e928d18b90a1fd
ZedTheoT <86628399+ZedTheoT@users.noreply.github.com>
2024-09-03 10:31:43 +0200

    update terraform

--- iac/cloudfront.tf
+++ iac/cloudfront.tf
@@ -1,22 +1,52 @@
 resource "aws_cloudfront_distribution" "cv-demo" {
 
   origin {
-    origin_id   = "s3-${aws_s3_bucket.cv-demo.bucket}-origin"
+    origin_id   = aws_s3_bucket.cv-demo.bucket_regional_domain_name
     domain_name = aws_s3_bucket_website_configuration.cv-demo.website_endpoint
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
   }
 
   default_cache_behavior {
+
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = aws_s3_bucket.cv-demo.bucket_regional_domain_name
+    viewer_protocol_policy = "allow-all"
+  }
+
+  ordered_cache_behavior {
+
+    path_pattern = "/*"
+    target_origin_id       = aws_s3_bucket.cv-demo.bucket_regional_domain_name
+    viewer_protocol_policy = "redirect-to-https"
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "s3-${aws_s3_bucket.cv-demo.bucket}-origin"
-    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+
+      query_string = false
+      headers      = ["Origin"]
+      cookies {
+
+        forward = "none"
+
+      }
+
+    }
   }
+
 
   enabled             = true
   is_ipv6_enabled     = false
   comment             = "CV demo distribution"
   default_root_object = "index.html"
   price_class         = "PriceClass_100"
+  http_version        = "http2and3"
 
   restrictions {
     geo_restriction {
@@ -28,26 +58,6 @@ resource "aws_cloudfront_distribution" "cv-demo" {
     cloudfront_default_certificate = true
   }
 
-  staging = var.environment == "test" ? true : false
+  #staging = var.environment == "test" ? true : false
 
-}
-
-resource "aws_cloudfront_continuous_deployment_policy" "cv-demo" {
-  enabled = var.environment == "test" ? true : false
-
-  staging_distribution_dns_names {
-    items    = [aws_cloudfront_distribution.cv-demo.domain_name]
-    quantity = 1
-  }
-
-  traffic_config {
-    type = "SingleWeight"
-    single_weight_config {
-      weight = "0.01"
-      session_stickiness_config {
-        idle_ttl    = 300
-        maximum_ttl = 600
-      }
-    }
-  }
 }

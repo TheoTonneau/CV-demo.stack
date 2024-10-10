f5c088b0ad27d525864dd461cd599650c47c727f
TheoTonneau <theo.tonneau@soregies.fr>
2024-10-10 15:29:06 +0200

    main Fix header

--- src/ts/header.ts
+++ src/ts/header.ts
@@ -51,13 +51,13 @@ export function updateNav(): void {
 
     pageNavButtons.forEach((button: Element) => {
         button.addEventListener('click', function (this: HTMLElement): void {
+            let newPage: string = this.dataset.pageselected;
+            setCurrentPage(newPage);
+            navigate(newPage, currentLang);
             const navBar: HTMLElement | null = document.getElementById('navBar');
             if (navBar) {
                 navBar.classList.toggle('');
             }
-            let newPage: string = this.dataset.pageselected;
-            setCurrentPage(newPage);
-            navigate(newPage, currentLang);
         });
     });
 

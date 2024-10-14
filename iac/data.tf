a7b71e0c09a0e1393808d07c01faaba5f2ce1e46
TheoTonneau <theo.tonneau@soregies.fr>
2024-10-14 09:39:44 +0200

    Modify_workflow&Terraform

--- iac/data.tf
+++ iac/data.tf
@@ -18,7 +18,7 @@ data "aws_iam_policy_document" "cloudfront_access_s3" {
 data "aws_acm_certificate" "cv_demo" {
   provider = aws.acm_global
   statuses = ["ISSUED"]
-  domain   = "cv-demo-${var.environment}.theo-tonneau.com"
+  domain   = [var.environment == "prod" ? "cv-demo.theo-tonneau.com" : "cv-demo-${var.environment}.theo-tonneau.com"]
 }
 
 data "aws_iam_policy_document" "cv_demo_assume_role" {

5b61442a6180d1970152a2ad17ab5a8c09d35cec
ZedTheoT <theo.tonneau@soregies.fr>
2024-11-04 14:47:50 +0100

    add cookie dark-mode

--- src/ts/dark-mode.ts
+++ src/ts/dark-mode.ts
@@ -1,6 +1,141 @@
 const toggleInput: HTMLInputElement = document.getElementById('toggle') as HTMLInputElement;
 
+function setCookie(name: string, value: string, days: number): void {
+    const date: Date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires: string = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name: string): string {
+    const cname: string = name + "=";
+    const decodedCookie: string = decodeURIComponent(document.cookie);
+    const cookieArray: string[] = decodedCookie.split(';');
+    for (let i: number = 0; i < cookieArray.length; i++) {
+        let c: string = cookieArray[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cname) === 0) {
+            return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
+
+window.onload = function(): void {
+    setTimeout(function() {
+        const darkModeCookie = getCookie('darkMode');
+        if (darkModeCookie === 'true') {
+            toggleInput.checked = true;
+            document.body.classList.add('dark-mode');
+            document.querySelector('footer').classList.add('dark-mode');
+            document.getElementById('header').classList.add('dark-mode');
+            document.getElementById('code-background').classList.add('dark-mode');
+            document.getElementById('introText-h1').classList.add('dark-mode');
+            document.getElementById('introText-p1').classList.add('dark-mode');
+            document.getElementById('introText-p2').classList.add('dark-mode');
+            document.getElementById('overview1').classList.add('dark-mode');
+            document.getElementById('overview2').classList.add('dark-mode');
+            document.getElementById('overview3').classList.add('dark-mode');
+            document.getElementById('overview1-p').classList.add('dark-mode');
+            document.getElementById('overview2-p').classList.add('dark-mode');
+            document.getElementById('overview3-p').classList.add('dark-mode');
+            document.getElementById('github-1').classList.add('dark-mode');
+            document.getElementById('github-2').classList.add('dark-mode');
+            document.getElementById('linkedinHeader').classList.add('dark-mode');
+            document.getElementById('linkedinContent').classList.add('dark-mode');
+            document.getElementById('linkedinProfile').classList.add('dark-mode');
+            document.getElementById('iconLinkedIn').classList.add('dark-mode');
+            document.getElementById('linkedinName').classList.add('dark-mode');
+            document.getElementById('linkedinPost').classList.add('dark-mode');
+            document.getElementById('linkedinInfos').classList.add('dark-mode');
+            document.getElementById('linkedInSchool').classList.add('dark-mode');
+            document.getElementById('linkedInCompany').classList.add('dark-mode');
+            document.getElementById('viewLinkedIn').classList.add('dark-mode');
+            document.getElementById('formEmail').classList.add('dark-mode');
+
+
+            document.querySelectorAll('.backAbout').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.title').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.text').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.text h3').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.text p').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.Iframe').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.illustration').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.cardExp').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.linkedinExp').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.expDetails').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.losangeExperiences').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.slides').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.links').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.button').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.logo-image').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.text-haut').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+
+            document.querySelectorAll('.text-bas').forEach((element: HTMLElement) => {
+                element.classList.add('dark-mode');
+            });
+        }
+        else {
+            toggleInput.checked = false;
+        }
+    }, 100);
+};
+
 toggleInput.addEventListener('change', (): void => {
+    if (toggleInput.checked) {
+        setCookie('darkMode', 'true', 365);
+    } else {
+        setCookie('darkMode', 'false', 365);
+    }
     document.body.classList.toggle('dark-mode', toggleInput.checked);
     document.querySelector('footer').classList.toggle('dark-mode', toggleInput.checked);
     document.getElementById('header').classList.toggle('dark-mode', toggleInput.checked);
--- src/ts/loadModule.ts
+++ src/ts/loadModule.ts
@@ -1,4 +1,5 @@
-fetch('./module/homeWrapper.html')
+setTimeout(function() {
+    fetch('./module/homeWrapper.html')
     .then((response: Response) => response.text())
     .then((data: string) => {
         const homeElement: HTMLElement | null = document.getElementById('home');
@@ -8,63 +9,64 @@ fetch('./module/homeWrapper.html')
     })
     .catch((error: Error) => console.error('Erreur de chargement:', error));
 
-fetch('./module/about.html')
-    .then((response: Response) => response.text())
-    .then((data: string) => {
-        const homeElement: HTMLElement | null = document.getElementById('about');
-        if (homeElement) {
-            homeElement.innerHTML = data;
-        }
-    })
-    .catch((error: Error) => console.error('Erreur de chargement:', error));
+    fetch('./module/about.html')
+        .then((response: Response) => response.text())
+        .then((data: string) => {
+            const homeElement: HTMLElement | null = document.getElementById('about');
+            if (homeElement) {
+                homeElement.innerHTML = data;
+            }
+        })
+        .catch((error: Error) => console.error('Erreur de chargement:', error));
 
 
-fetch('./module/skills.html')
-    .then((response: Response) => response.text())
-    .then((data: string) => {
-        const homeElement: HTMLElement | null = document.getElementById('skills');
-        if (homeElement) {
-            homeElement.innerHTML = data;
-        }
-    })
-    .catch((error: Error) => console.error('Erreur de chargement:', error));
+    fetch('./module/skills.html')
+        .then((response: Response) => response.text())
+        .then((data: string) => {
+            const homeElement: HTMLElement | null = document.getElementById('skills');
+            if (homeElement) {
+                homeElement.innerHTML = data;
+            }
+        })
+        .catch((error: Error) => console.error('Erreur de chargement:', error));
 
-fetch('./module/formation.html')
-    .then((response: Response) => response.text())
-    .then((data: string) => {
-        const homeElement: HTMLElement | null = document.getElementById('formations');
-        if (homeElement) {
-            homeElement.innerHTML = data;
-        }
-    })
-    .catch((error: Error) => console.error('Erreur de chargement:', error));
+    fetch('./module/formation.html')
+        .then((response: Response) => response.text())
+        .then((data: string) => {
+            const homeElement: HTMLElement | null = document.getElementById('formations');
+            if (homeElement) {
+                homeElement.innerHTML = data;
+            }
+        })
+        .catch((error: Error) => console.error('Erreur de chargement:', error));
 
-fetch('./module/experiences.html')
-    .then((response: Response) => response.text())
-    .then((data: string) => {
-        const homeElement: HTMLElement | null = document.getElementById('experiences');
-        if (homeElement) {
-            homeElement.innerHTML = data;
-        }
-    })
-    .catch((error: Error) => console.error('Erreur de chargement:', error));
+    fetch('./module/experiences.html')
+        .then((response: Response) => response.text())
+        .then((data: string) => {
+            const homeElement: HTMLElement | null = document.getElementById('experiences');
+            if (homeElement) {
+                homeElement.innerHTML = data;
+            }
+        })
+        .catch((error: Error) => console.error('Erreur de chargement:', error));
 
-fetch('./module/website.html')
-    .then((response: Response) => response.text())
-    .then((data: string) => {
-        const homeElement: HTMLElement | null = document.getElementById('website');
-        if (homeElement) {
-            homeElement.innerHTML = data;
-        }
-    })
-    .catch((error: Error) => console.error('Erreur de chargement:', error));
+    fetch('./module/website.html')
+        .then((response: Response) => response.text())
+        .then((data: string) => {
+            const homeElement: HTMLElement | null = document.getElementById('website');
+            if (homeElement) {
+                homeElement.innerHTML = data;
+            }
+        })
+        .catch((error: Error) => console.error('Erreur de chargement:', error));
 
-fetch('./module/contact.html')
-    .then((response: Response) => response.text())
-    .then((data: string) => {
-        const homeElement: HTMLElement | null = document.getElementById('contact');
-        if (homeElement) {
-            homeElement.innerHTML = data;
-        }
-    })
-    .catch((error: Error) => console.error('Erreur de chargement:', error));
+    fetch('./module/contact.html')
+        .then((response: Response) => response.text())
+        .then((data: string) => {
+            const homeElement: HTMLElement | null = document.getElementById('contact');
+            if (homeElement) {
+                homeElement.innerHTML = data;
+            }
+        })
+        .catch((error: Error) => console.error('Erreur de chargement:', error));
+}, 100);

87c82cbf58a9ee1c9641ceed3e7cfe325c6070df
ZedTheoT <theo.tonneau@soregies.fr>
2024-11-06 11:11:19 +0100

    animation of mobile burger

--- src/ts/mobile.ts
+++ src/ts/mobile.ts
@@ -4,7 +4,9 @@ burger.addEventListener('click', (): void => {
     const ul: HTMLInputElement = document.getElementById('ulMobile') as HTMLInputElement;
     if (ul.style.opacity === '1') {
         ul.style.opacity = '0';
+        burger.classList.remove('active');
     } else {
         ul.style.opacity = '1';
+        burger.classList.add('active');
     }
 });
